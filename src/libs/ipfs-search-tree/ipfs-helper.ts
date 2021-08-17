1ef0a9a192348a36a439c263e01efaf4ed42e443
ZeframLou <zeframlou@gmail.com>
2021-08-16 22:07:59 -0700

    Switch to pinata for IPFS uploading

--- src/libs/ipfs-search-tree/ipfs-helper.ts
+++ src/libs/ipfs-search-tree/ipfs-helper.ts
@@ -1,10 +1,21 @@
 import IPFS from '../ipfs-mini';
+import Pinata from '@pinata/sdk';
 
 export class IPFSHelper {
+  PINATA_KEY_PUBLIC = 'a4f28fad3d730470b5d4';
+  PINATA_KEY_PRIVATE =
+    'ce41cd7a93892a02542a08897a6372500e3784d2ef2d323c743a0a9582709619';
+
+  pinata: any;
   ipfs: any;
 
   constructor(ipfsEndpoint: string) {
-    this.ipfs = new IPFS({ host: ipfsEndpoint, protocol: 'https', base: '/api/v0' });
+    this.pinata = Pinata(this.PINATA_KEY_PUBLIC, this.PINATA_KEY_PRIVATE);
+    this.ipfs = new IPFS({
+      host: ipfsEndpoint,
+      protocol: 'https',
+      base: '/api/v0',
+    });
   }
 
   async getObjectFromIPFS(ipfsHash: string | null): Promise<any> {
@@ -22,15 +33,13 @@ export class IPFSHelper {
     });
   }
 
-  uploadObjectToIPFS(value: any): Promise<string> {
-    return new Promise((resolve, reject) => {
-      this.ipfs.addJSON(value, (err, result) => {
-        if (err != null) {
-          reject(err);
-        } else {
-          resolve(result);
-        }
-      });
-    });
+  async uploadObjectToIPFS(value: any): Promise<string> {
+    const options = {
+      pinataOptions: {
+        cidVersion: 0,
+      },
+    };
+    const response = await this.pinata.pinJSONToIPFS(value, options);
+    return response.IpfsHash;
   }
 }
