9b2d17b2a0fbcbfcd4da8442a19a9e92d0bcbb17
Adam Gall <galladamj@gmail.com>
2022-01-17 16:07:49 -0500

    Create accountName variable, and a setter method that will do ENS lookup

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WalletService } from '../wallet.service';
+import { ethers } from 'ethers';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +8,27 @@ import { WalletService } from '../wallet.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  accountName: string;
 
   constructor(public wallet: WalletService) { }
 
   ngOnInit(): void {
+    this.accountName = "unknown";
   }
 
   connectWallet() {
     this.wallet.connect(() => { }, () => { }, false);
+  }
+
+  setAccountName(address: string): void {
+    this.accountName = `${address.substring(0, 6)}...${address.slice(-4)}`
+
+    const ethersProvider = new ethers.providers.Web3Provider(this.wallet.web3.givenProvider);
+    ethersProvider.lookupAddress(address)
+      .then(lookedupAddress => {
+        if (lookedupAddress) {
+          this.accountName = lookedupAddress;
+        }
+      })
   }
 }
